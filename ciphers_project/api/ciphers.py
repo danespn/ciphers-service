5d092ebc87ead3b8d715ee8dbd5785924fc66a37
Spencer, Dane Sullivan <danespen@iu.edu>
2024-02-29 15:27:10 -0500

    practicum 8 changes

--- ciphers_project/api/ciphers.py
+++ ciphers_project/api/ciphers.py
@@ -3,9 +3,13 @@
 def caesar_encode(plain_text, shift):
     cipher_text = ""
     for c in plain_text:
-        c_encode = ord(c) + shift
-        if(c_encode > ord('z')):
-            c_encode = c_encode % 26
-        c_encode = chr(c_encode)
+        if c.isalpha():
+            if c.islower():
+                c_encode = (ord(c) - ord('a') + shift) % 26 + ord('a')
+            else:
+                c_encode = (ord(c) - ord('A') + shift) % 26 + ord('A')
+            c_encode = chr(c_encode)
+        else:
+            c_encode = c
         cipher_text += c_encode
     return cipher_text
